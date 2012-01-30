b44dde74ca00309ec0ee0df8ad4a79c9dd83850e
Ryan Doom <ryan.doom@webascender.com>
2012-01-29 20:41:45 -0500

    version 1

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,21 @@
+class HomeController < ApplicationController
+  
+  def index
+    
+  end
+  
+  def generate
+    data = params[:q]
+    @converted = ""
+    data.split("\r\n").each_with_index do |row,i|      
+      from,to = row.split(',')
+      @converted += "<rule name=\"generated-#{i}\" stopProcessing=\"true\">\r\n" +
+      "  <match url=\".*\" />\r\n" +
+      "    <conditions logicalGrouping=\"MatchAny\">\r\n" +
+      "      <add input=\"{QUERY_STRING}\" pattern=\".*#{from.strip}.*\" />\r\n" +
+      "    </conditions>\r\n" +
+      "    <action type=\"Redirect\" appendQueryString=\"false\" url=\"#{to.strip}\" redirectType=\"Permanent\" />\r\n" +
+      "</rule>\r\n" + "\r\n"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Iis7Rewrite::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+
+  #resources :home
+  
+  match 'generate' => 'home#generate'
+  
+  
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -48,7 +54,7 @@ Iis7Rewrite::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
